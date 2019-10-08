c5c2b5d71737113053bfc1abd4cdea537bda96d6
fabioCalcagno <fabio.calcagno@fatec.sp.gov.br>
2019-10-08 20:49:32 -0300

    layout-login-90%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component';
 
 import { LessonsComponent } from '../app/lessons/lessons.component';
 
 
 const routes: Routes = [
-  {path: 'lessons' ,
-   component: LessonsComponent}
+  {
+    path: 'lessons',
+    component: LessonsComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LessonsComponent } from './lessons/lessons.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LessonsComponent
+    LessonsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
