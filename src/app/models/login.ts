36c93804f5b0bfa1406987cd7cfd5dc09f186bc2
rfabais <54381216+rfabais@users.noreply.github.com>
2019-10-25 18:01:08 -0300

    Organizacao master

--- src/app/models/login.ts
+++
@@ -1,9 +0,0 @@
-export  class  User {
-                
-        constructor(
-                public user    : string = "",
-                public email   : string = "",
-                public password: string = "",
-        ){}
-
-}
