1e3a2296f5c94ebae1ea947b5c2f968c77ee7519
Fabio <fabio.calcagno@fatec.sp.gov.br>
2019-11-09 17:47:07 -0300

    puxa essa ricardo

--- jogo-app-front/src/app/login/criar-conta/criar-conta.component.ts
+++ jogo-app-front/src/app/login/criar-conta/criar-conta.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { User } from '../../models/User';
+import { IUser } from '../../models/User';
 import { LoginService } from '../services/login/login.service'
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
